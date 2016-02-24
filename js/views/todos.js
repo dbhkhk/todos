4f610a50dceb594d3fbffcb081b1e919f1975df6
Brody Chen <rufengch@gmail.com>
2016-02-23 16:41:22 -0800

    feat: Add completing and deleting features

--- js/views/todos.js
+++ js/views/todos.js
@@ -8,6 +8,8 @@ app.TodoView = Backbone.View.extend({
 	template: _.template( $('#item-template').html() ),
 
 	events: {
+		'click .toggle':  'toggleCompleted',
+		'click .destroy': 'clear',
 		'dbclick label':  'edit', // double click on item title to edit the title
 		'keypress .edit': 'updateOnEnter', // either hit enter or 'blur' input will call updateOnEnter
 		'blur .edit':     'close'
@@ -17,12 +19,34 @@ app.TodoView = Backbone.View.extend({
 		// {model: todo} will be passed in when a TodoView is created
 		// listen to model's change and render
 		this.listenTo(this.model, 'change', this.render);
+		this.listenTo(this.model, 'destroy', this.remove);
+		this.listenTo(this.model, 'visible', this.toggleVisible);
 	},
 
 	render: function(){
 		this.$el.html( this.template( this.model.attributes ) );
+
+		this.$el.toggleClass('completed', this.model.get('completed'));
+		this.toggleVisible();
+
 		this.$input = this.$('.edit'); // update this.$input once render is called
 		return this;
+	},
+
+	toggleVisible: function(){
+		this.$el.toggleClass('hidden', this.isHidden());
+	},
+
+	isHidden: function(){
+		var isCompleted = this.model.get('completed');
+		return (
+			(!isCompleted && app.TodoFilter === 'completed')
+			|| (isCompleted && app.TodoFilter === 'active')
+		);
+	},
+
+	toggleCompleted: function(){
+		this.model.toggle();
 	},
 
 	edit: function(){
@@ -45,5 +69,9 @@ app.TodoView = Backbone.View.extend({
 		if (e.which === ENTER_KEY) {
 			this.close();
 		}
+	},
+
+	clear: function(){
+		this.model.destroy();
 	}
 });
